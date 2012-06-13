6d13f105a71f1edbbfea3daf1aac3b88e15945b0
diego <diego@gcoop.coop>
2012-06-13 14:35:31 -0300

    avanzamos con geolocalizar, hay 300

--- geolocalizar.py
+++ geolocalizar.py
@@ -6,24 +6,33 @@ from googlemaps import GoogleMaps
 
 gmaps = GoogleMaps('ABQIAAAAbwWYBdNDWhSrlUDhnHoOzhRSZLIMLGsAILtzGCo_JdRkl29wtRTgPiY-xKbabP9VEHL8y3eZuIHvPw')
 
-#CALLE, LOCALIDAD, ERROR = range(3)
-
 coops = json.load(codecs.open('./datos/Cooperativas.json', 'r', 'utf-8'))['rows']
-fallidas = 0
+calle, localidad , pais = 0,0,0
 
 salida = []
 
-for i, coop in enumerate(coops[:100]):
-    direccion = "%s %s" % (coop['Localidad'], coop['PROVINCIA'])
+for i, coop in enumerate(coops[:300]):
+    direccion = "%s, %s %s" % (coop['Direccion'], coop['Localidad'], coop['PROVINCIA'])
     try:
         lat, lng = gmaps.address_to_latlng(direccion)
+        precision = 'calle'
+        calle += 1
     except:
-        fallidas += 1
-        lat,lng = -34.506132,-58.162336 #Rio de La Plata
+        direccion = "%s %s" % (coop['Localidad'], coop['PROVINCIA'])
+        try:
+            lat, lng = gmaps.address_to_latlng(direccion)
+            precision = 'localidad'
+            localidad += 1
+        except:
+            lat,lng = -34.506132,-58.162336 #Rio de La Plata
+            precision = 'pais'
+            pais += 1
+
     #print direccion,lat,lng
-    print "fallaron %d / %d\r" % (fallidas, i+1),
+    print "\r%d / %d / %d" % (calle, localidad, pais),
     sys.stdout.flush()
     coop['lat'], coop['long'] = lat, lng
+    coop['precision'] = precision
     salida.append(coop)
 
 json.dump({'rows': salida}, codecs.open('./datos/Cooperativas_geo.json', 'w', 'utf-8'), indent=0, ensure_ascii=False, encoding='utf-8')
